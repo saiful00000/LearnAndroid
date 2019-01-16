187b2ee3762c601c91eba65f4c2979e0b5bf2b8d
saiful00000 <>
2019-01-17 01:04:21 +0600

    Common widget contents all layout created and itemclick implemented

--- app/src/main/java/com/example/saifulislam/learnandroid/CommonWidgetActivity.java
+++ app/src/main/java/com/example/saifulislam/learnandroid/CommonWidgetActivity.java
@@ -1,8 +1,11 @@
 package com.example.saifulislam.learnandroid;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -10,16 +13,28 @@ import android.widget.Toast;
 
 import com.example.saifulislam.learnandroid.adapters.WidgetsListViewAdapter;
 
-import java.util.List;
-
 public class CommonWidgetActivity extends AppCompatActivity {
 
     ListView listView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_common_widget);
+
+        //toolbar
+        toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(Color.WHITE);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
 
         listView = findViewById(R.id.widgetsListViewId);
         String[] contents = getResources().getStringArray(R.array.common_widget_list);
@@ -35,45 +50,63 @@ public class CommonWidgetActivity extends AppCompatActivity {
             }
         });
 
-
     }
 
     private void itemClickHelper(int position) {
         Intent intent;
         switch (position) {
             case 0 ://textview
-                Toast.makeText(this, "textView", Toast.LENGTH_SHORT).show();
+                intent = new Intent(this, TextViewActivity.class);
+                startActivity(intent);
                 break;
             case 1://edittext:
-                Toast.makeText(this, "editText", Toast.LENGTH_SHORT).show();
+                intent = new Intent(this, EditTextActivity.class);
+                startActivity(intent);
                 break;
             case 2://button:
-                Toast.makeText(this, "Button", Toast.LENGTH_SHORT).show();
+                intent = new Intent(this, ButtonActivity.class);
+                startActivity(intent);
                 break;
             case 3://image button
-                Toast.makeText(this, "imageButton", Toast.LENGTH_SHORT).show();
+                intent = new Intent(this, ImageButtonActivity.class);
+                startActivity(intent);
                 break;
             case 4://switcher
-                Toast.makeText(this, "Switcher", Toast.LENGTH_SHORT).show();
+                intent = new Intent(this, SwitchActivity.class);
+                startActivity(intent);
                 break;
             case 5://checkbox
-                Toast.makeText(this, "checkbox", Toast.LENGTH_SHORT).show();
+                intent = new Intent(this, CheckBoxActivity.class);
+                startActivity(intent);
                 break;
             case 6://alertdialog
-                Toast.makeText(this, "alertDialog", Toast.LENGTH_SHORT).show();
+                intent = new Intent(this, AlertDialogActivity.class);
+                startActivity(intent);
                 break;
             case 7://scrollview
-                Toast.makeText(this, "scrollView", Toast.LENGTH_SHORT).show();
+                intent = new Intent(this, ScrollViewActivity.class);
+                startActivity(intent);
                 break;
             case 8://progressbar
-                Toast.makeText(this, "ProgresBar", Toast.LENGTH_SHORT).show();
+                intent = new Intent(this, ProgressBarActivity.class);
+                startActivity(intent);
                 break;
             case 9://seekbar
-                Toast.makeText(this, "Seekbar", Toast.LENGTH_SHORT).show();
+                intent = new Intent(this, SeekBarActivity.class);
+                startActivity(intent);
                 break;
             case 10://radiobutton
-                Toast.makeText(this, "radioButton", Toast.LENGTH_SHORT).show();
+                intent = new Intent(this, RadioButtonActivity.class);
+                startActivity(intent);
                 break;
         }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.other_activity_manu, menu);
+        return true;
     }
 }
