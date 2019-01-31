961062dd0ec9b06ecb9917e994a59039b729e136
saiful00000 <>
2019-02-01 02:20:59 +0600

    action bar aded to all activity for main activity

--- app/src/main/java/com/example/saifulislam/learnandroid/RadioButtonActivity.java
+++ app/src/main/java/com/example/saifulislam/learnandroid/RadioButtonActivity.java
@@ -32,4 +32,5 @@ public class RadioButtonActivity extends AppCompatActivity {
             }
         });
     }
+
 }
--- app/src/main/java/com/example/saifulislam/learnandroid/SQLiteActivity.java
+++ app/src/main/java/com/example/saifulislam/learnandroid/SQLiteActivity.java
@@ -1,13 +1,37 @@
 package com.example.saifulislam.learnandroid;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class SQLiteActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sqlite);
+
+        //setup the toolbar
+        setUpTheToolBar();
+
+    }
+
+
+    //setup the toolbar
+    private void setUpTheToolBar() {
+        toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(Color.WHITE);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
